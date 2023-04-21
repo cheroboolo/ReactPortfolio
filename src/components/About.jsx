4485b117836b114a64fee1e8cc26df972df84d00
Neo <cheroboolo@gmail.com>
2023-04-21 14:54:50 +0200

    initial about image change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { motion } from "framer-motion";
 import SlavisaCV from "../cv/SlavisaCV.pdf"
+import Neo from "../images/neo.jpg"
 
 const buttonVariant = {
     hover: {
@@ -37,7 +38,7 @@ function About() {
         
         <motion.div className="about-main" variants={aboutVariants} initial="hidden" whileInView="visible" viewport={{once:true}}>
             <motion.h1 variants={buttonVariant} whileHover="hover">About</motion.h1>
-            <motion.img className="about-img" src="https://scontent.fbeg5-1.fna.fbcdn.net/v/t1.6435-9/40527511_10215835334011611_2108058446677934080_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=174925&_nc_ohc=B6uweDhpWDoAX9DKqa9&_nc_ht=scontent.fbeg5-1.fna&oh=00_AfAO9C0fvKuiyHWsU_WAOJjHTa9jYZH12XOYomVl0yRjkw&oe=64414C25" alt="neo.jpg" initial={{x:"100vw"}} animate={{x:0}} transition={{type:"spring", delay:2, duration:3}}></motion.img>
+            <motion.img className="about-img" src={Neo} alt="neo.jpg" initial={{x:"100vw"}} animate={{x:0}} transition={{type:"spring", delay:2, duration:3}}></motion.img>
             <div className="about-text">
                 <motion.p className="left-content" initial={{opacity:0, scale:0}} whileInView={{opacity:1, scale:1}} transition={{type:"spring", delay:1, duration:2}}>I'm quietly confident, naturally curious, and perpetually working on improving my knowledge of Python and JavaScript. Learning new tools and programming language to use logic and experience to level up as Full-Stack Developer.</motion.p>
                 <motion.p className="right-content" initial={{opacity:0, scale:0}} whileInView={{opacity:1, scale:1}} transition={{type:"spring", delay:0.6, duration:3}}>Since beginning my journey as a freelance Python Developer nearly 1 year ago, I've done some small Web Scraping projects with tools such Beautiful Soup, Selenium, Data Visualation with MatLab, PX, Seaborn, Automation with Pandas and Requests, on Upwork.</motion.p>
